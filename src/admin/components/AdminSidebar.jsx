0453116d1c5713e7f494bf9ca91a6a3cb0ad0453
RANA <ranazika2003@gmail.com>
2025-03-17 15:21:57 +0200

    added admin sidebar components

---
+++ src/admin/components/AdminSidebar.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaTachometerAlt, FaBook, FaUser } from "react-icons/fa";
+import './AdminSidebar.css';
+
+const AdminSidebar = () => {
+
+  const [isBooksOpen, setIsBooksOpen] = useState(false);
+  const [isUsersOpen, setIsUsersOpen] = useState(false); 
+
+  return (
+    <div className="sidebar">
+      <ul>
+        <ul className="manageBook">
+        <li onClick={() => setIsBooksOpen(!isBooksOpen)}>
+          <FaBook/>
+          <span>  Books</span>
+          {isBooksOpen && (
+            <ul>
+              <li><Link to="/admin/addbook">Add Book</Link></li>
+              <li><Link to="/admin/managebook">Manage Books</Link></li>
+            </ul>
+          )}
+        </li>
+        </ul>
+        <ul className="manageUser">
+        <li onClick={() => setIsUsersOpen(!isUsersOpen)}>
+          <FaUser/>
+          <span>  Users</span>
+          {isUsersOpen && (
+            <ul >
+              <li><Link to="../pages/AddBook">Add User</Link></li>
+            </ul>
+          )}
+        </li>
+        </ul>
+      </ul>
+    </div>
+  );
+}
+
+export default AdminSidebar;
