fe6d91e0d6c6c8c6374a88d276dee59786cc6852
MohammedAmr04 <mohammedamr200497@gmail.com>
2025-04-08 15:41:35 +0200

    Create Landing Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import { Container } from "react-bootstrap";
 import BookSlider from "./../components/common/book-slider/BookSlider";
+import Landing from "./../components/common/landing/Landing";
 
 export default function Home() {
   return (
-    <div className="py-5">
+    <div>
+      <Landing />
+
       <Container>
         <BookSlider />
         <BookSlider />
